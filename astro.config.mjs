4d332ba2e6eb213904a7511bcefe4fdc6be03ce0
xirf <itsandka@gmail.com>
2024-06-26 23:02:25 +0700

    header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import Unocss from "unocss/astro";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [ mdx(), sitemap(), Unocss({
+    injectReset: true,
+  })],
   output: "server",
   adapter: cloudflare()
 });
